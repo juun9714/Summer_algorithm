03f135db5345cd40233d540d0a12ba53172f585f
juun9714 <lovekwon200@gmail.com>
2022-06-15 15:51:29 +0900

    2nd week revised

--- code/15700.py
+++ code/15700.py
@@ -1,18 +1,2 @@
 N,M=map(int,(input().split()))
-
-total=0
-
-if N==1 and M==1:
-    total=0
-elif N==1 and M!=1:
-    total=M//2
-elif N!=1 and M==1:
-    total=N//2
-elif N%2==0:
-    total=(N//2)*M
-elif N%2!=0 and M%2==0:
-    total=(M//2)*N
-elif N%2!=0 and M%2!=0:
-    total=(N//2)*M+(M//2)
-
-print(int(total))
+print((M*N)//2)
