1679c996eb6a7d05483eea46c3cfef2fbdf11fc7
juun9714 <lovekwon200@gmail.com>
2022-06-20 20:55:50 +0900

    silver

--- code/4673.py
+++ code/4673.py
@@ -1,25 +1,14 @@
-start=1
-arr=[tmp for tmp in range(10000)]
-print(arr)
-while True:
+arr=[tmp for tmp in range(1,10001)]
+for start in range(1,10001):
     selfNum=start
+
     for i in range(len(str(start))):
         selfNum+=int(str(start)[i])
-    if selfNum>9999:
-        break
-    else:
-        print(selfNum)
-        if selfNum in arr:
-            arr.remove(selfNum)
-        start+=1
 
 
-#for p in range(len(arr)):
-    #print(arr[p])
+    if selfNum in arr:
+        arr.remove(selfNum)
 
-# A=3345
-# selfNum=A
-# for i in range(len(str(A))):
-#     print(int(str(A)[i]))
-#     selfNum+=int(str(A)[i])
-# print(selfNum)
+
+for i in range(len(arr)):
+    print(arr[i])
