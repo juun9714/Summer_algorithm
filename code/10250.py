4e4cccdc936138ac87bda1898f527a10ed84965b
권준희 <june@gwonjunhuiui-MacBookAir.local>
2022-06-21 20:36:32 +0900

    3rd, 1 prob

---
+++ code/10250.py
@@ -0,0 +1,18 @@
+it=int(input())
+
+for _ in range(it):
+    H, W, N=map(int,input().split())
+    ho=N//H
+    flr=N%H
+
+    if flr==0:
+        if ho>=10:
+            print(str(H)+str(ho))
+        else:
+            print(str(H)+"0"+str(ho))
+    else:
+        if ho+1>=10:
+            print(str(flr)+str(ho+1))
+        else:
+            print(str(flr)+"0"+str(ho+1))
+        
