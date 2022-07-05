758ed386a6289a1597c8eebfb84f21854acd15ad
juun9714 <lovekwon200@gmail.com>
2022-07-05 18:56:21 +0900

    castle done

--- code/5thWeek/1236.py
+++ code/5thWeek/1236.py
@@ -3,18 +3,26 @@ import sys
 N, M = map(int, sys.stdin.readline().split())
 castle=[['.' for _ in range(M)] for _ in range(N)]
 for n in range(N):
-    castle[n]=list(map(str, sys.stdin.readline().split()))
-
+    castle[n]=list(sys.stdin.readline().rstrip())
+print(castle)
 for n in range(N):
     if 'X' not in castle[n]:
         print("X is not castle[{}]".format(n))
-        check=0
-
-        for sero in range(M):
-            for garo in range(N):
-                if castle[garo][sero]!='X':
-                    #print("castle[{}][{}] is empty!! {}".format(garo,sero,castle[garo][sero]))
-                    castle[n][sero]='X'
+        for mm in range(M):
+            check=-1
+            for nn in range(N):
+                if castle[nn][mm]=='X':
+                    check=1
+                    break
+            if(check==-1):
+                print("check is {} and X is added to castle[{}][{}]".format(check,n,mm))
+                castle[n][mm]='X'
                 break
-            break
-    print(castle)
+
+for n in range(N):
+    if 'X' not in castle[n]:
+        print("X is added to castle[{}][{}]".format(n,0))
+        castle[n][0]='X'
+
+
+print(castle)
